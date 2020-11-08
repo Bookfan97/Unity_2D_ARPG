80315a36dc16baaeb7f25b26e1a3004e9475cb8f
Noah Day <sheltielover432@gmail.com>
2020-11-07 23:06:22 -0500

    Physics and Rigidbody

--- Action RPG/Assets/Scripts/PlayerController.cs
+++ Action RPG/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 8;
+    [SerializeField] private Rigidbody2D _rigidbody2D = null;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,10 +15,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(
+        /*transform.position = new Vector3(
             transform.position.x + (Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime),
             transform.position.y + (Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime),
             0f
-            );
+            );*/
+        _rigidbody2D.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * moveSpeed;
     }
 }
